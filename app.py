874101dc6719cbec61bac2c00ee74e4eab5fddf9
LZ <2327433383@qq.com>
2023-04-23 06:57:05 +0800

    创建新页面

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
+@app.route('/index')
+def index():  # put application's code here
+    user = {'username':'crazy'}
+    return render_template('page1.html',title='home',user=user)
 
 if __name__ == '__main__':
     app.run()
