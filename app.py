c7150fc98c4ae38ae674405fac5eefa836e7a394
LZ <2327433383@qq.com>
2023-04-25 05:01:44 +0800

    创建模板页面

--- app.py
+++ app.py
@@ -8,7 +8,15 @@ app = Flask(__name__)
 @app.route('/index')
 def index():  # put application's code here
     user = {'username':'crazy'}
-    return render_template('page1.html',title='home',user=user)
+    posts =[{
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }]
+    return render_template('index.html',title='home',user=user,posts=posts)
 
 if __name__ == '__main__':
     app.run()
